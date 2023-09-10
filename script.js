8ab968dd3cf9811f22b8e8ba3f9dbf6b8f9a18df
Ellis-D13 <135081698+Ellis-D13@users.noreply.github.com>
2023-09-10 13:05:49 -0500

    included new code block supports json server

--- script.js
+++ script.js
@@ -1,38 +1,79 @@
-let tasks = []; // Global array to hold tasks
-
-// Function to fetch and display tasks
+// Function to fetch tasks from the API and display them
 function fetchTasks() {
-  let taskList = "<ul>";
-  tasks.forEach((task, index) => {
-    taskList += `<li>${task} <button onclick="deleteTask(${index})">Delete</button> <button onclick="editTask(${index})">Edit</button></li>`;
+  $.ajax({
+    url: "http://localhost:3000/messages", // Replace with your API URL
+    method: "GET",
+    success: function(data) {
+      let taskList = "<ul>";
+      data.forEach((task, index) => {
+        taskList += `<li>${task.content} <button onclick="deleteTask(${task.id})">Delete</button> <button onclick="editTask(${task.id})">Edit</button></li>`;
+      });
+      taskList += "</ul>";
+      $("#tasks").html(taskList); // Update the UI
+    },
   });
-  taskList += "</ul>";
-  document.getElementById("tasks").innerHTML = taskList;
 }
 
-// Initialize by fetching existing tasks
-fetchTasks();
+// Function to add a new task
+function addTask(taskContent) {
+  $.ajax({
+    url: "http://localhost:3000/messages", // Replace with your API URL
+    method: "POST",
+    data: { content: taskContent },
+    success: function(data) {
+      fetchTasks(); // Refresh the list after adding
+    },
+  });
+}
 
-document.getElementById("taskForm").addEventListener("submit", function(e) {
-  e.preventDefault(); // Prevent default form submit action
-  const taskContent = document.getElementById("taskInput").value;
-  tasks.push(taskContent);
-  fetchTasks();
-  document.getElementById("taskInput").value = '';
-});
-
-
-// Function to edit a task
-function editTask(index) {
-  const newTask = prompt("Edit your task:", tasks[index]);
-  if (newTask !== null) {
-    tasks[index] = newTask;
-    fetchTasks();
+// Function to edit an existing task
+function editTask(id) {
+  const newContent = prompt("Edit your task:");
+  if (newContent !== null) {
+    $.ajax({
+      url: `http://localhost:3000/messages/${id}`, // Replace with your API URL and task ID
+      method: "PUT",
+      data: { content: newContent },
+      success: function(data) {
+        fetchTasks(); // Refresh the list after editing
+      },
+    });
   }
 }
 
 // Function to delete a task
-function deleteTask(index) {
-  tasks.splice(index, 1); // Remove the task at the given index
-  fetchTasks(); // Refresh the task list
+function deleteTask(id) {
+  $.ajax({
+    url: `http://localhost:3000/messages/${id}`, // Replace with your API URL and task ID
+    method: "DELETE",
+    success: function(data) {
+      fetchTasks(); // Refresh the list after deleting
+    },
+  });
 }
+
+$("#taskForm").submit(function(e) {
+  e.preventDefault(); // Prevent default form behavior
+  const taskContent = $("#taskInput").val().trim(); // Grab the task input and trim any white space
+  
+  if(taskContent === '') {
+    // Inform the user that the task cannot be empty
+    alert('Task cannot be empty.');
+    return;
+  }
+
+  // AJAX POST request to create a new task
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:3000/messages", // API endpoint
+    data: JSON.stringify({ content: taskContent }), // Task content
+    contentType: "application/json",
+    success: function() {
+      fetchTasks(); // Fetch tasks again to update the view
+    }
+  });
+});
+
+
+// Initialize by fetching existing tasks
+fetchTasks();
